28a19a3e18d6cefeb02855f1ee22d48f54a38f54
waka <wakame0912@gmail.com>
2020-05-30 15:47:41 +0900

    initial commit

---
+++ src/editor.js
@@ -0,0 +1,179 @@
+const fs = remote.require('fs');
+const { 
+    BrowserWindow,
+    dialog 
+} = remote;
+
+let inputArea = null;
+let inputTxt = null;
+let footerArea = null;
+
+
+let currentPath = "";
+let editor = null;
+
+window.addEventListener('DOMContentLoaded', onLoad);
+
+function onLoad() {
+    inputArea = document.getElementById('input_area');
+    inputTxt = document.getElementById('input_txt');
+    footerArea = document.getElementById('footer_fixed');
+
+    editor = ace.edit('input_txt');
+    editor.setTheme("ace/theme/dracula");
+    editor.focus();
+    editor.gotoLine(1);
+    editor.renderer.setShowPrintMargin(false);
+
+    setEditorTheme();
+
+    document.addEventListener('dragover', (event) => {
+        event.preventDefault();
+    });
+    document.addEventListener("drop", (event) => {
+        event.preventDefault();
+    });
+
+    inputArea.addEventListener("dragover", (event) => {
+        event.preventDefault();
+    });
+    inputArea.addEventListener("dragleave", (e) => {
+        e.preventDefault();
+    });
+    inputArea.addEventListener("dragend", (e) => {
+        e.preventDefault();
+    });
+    inputArea.addEventListener("drop", (e) => {
+        e.preventDefault();
+        const file = e.dataTransfer.files[0];
+        readFile(file.path);
+    });
+
+    document.querySelector("#btnLoad").addEventListener("click", () => {
+        openLoadFile();
+    });
+    document.querySelector("#btnSave").addEventListener("click", () => {
+        saveFile();
+    });
+};
+
+function openLoadFile() {
+    const win = BrowserWindow.getFocusedWindow();
+    dialog.showOpenDialog(
+        win,
+        {
+            properties: ["openFile"],
+            filters: [
+                {
+                    name: "Documents",
+                    extensions: ["*"]
+                }
+            ]
+        }).then(result => {
+            if(!result.canceled){
+                readFile(result.filePaths[0]);
+            }
+        }).catch(err => {
+            console.log(err)
+        });
+}
+
+function readFile(path) {
+    currentPath = path;
+    fs.readFile(path, (error, text) =>{
+        if(error != null){
+            alert("error: " + error);
+            return;
+        }
+        footerArea, innerHTML = path;
+        editor.setValue(text.toString(), -1);
+        setEditorTheme(path);
+    });
+}
+
+function saveFile() {
+    if(currentPath === ""){
+        saveNewFile();
+        return;
+    }
+    const win = BrowserWindow.getFocusedWindow();
+
+    dialog.showMessageBox(win, {
+        title: "ファイルの上書き保存を行います",
+        type: "info",
+        buttons: ["OK", "Cancel"],
+        detail: "保存実行しますか？"
+    }).then(result => {
+        if(result.response === 0){
+            const data = editor.getValue();
+            writeFile(currentPath, data);
+        }
+    }).catch(err => {
+        console.log(err);
+    });
+}
+
+function writeFile(path, data){
+    fs.writeFile(path, data, (err) => {
+        if(err != null){
+            alert("error: " + err);
+        }else{
+            setEditorTheme(path);
+        }
+    });
+}
+
+function saveNewFile() {
+    const win = BrowserWindow.getFocusedWindow;
+    dialog.showSaveDialog(
+        win,
+        {
+            properties: ["openFile"],
+            filters: [{
+                name: "Documents",
+                extensions: ["*"]
+            }]
+        }).then(result => {
+            if(!result.canceled){
+                const data = editor.getValue();
+                currentPath = result.filePath;
+                writeFile(currentPath, data);
+            }
+        }).catch(err => {
+            console.log(err);
+        });
+}
+
+
+function setEditorTheme(fileName = ""){
+    const type = fileName.split(".");
+    const ext = type[type.length - 1].toLowerCase();
+    switch(ext) {
+        case 'txt':
+            editor.getSession().setMode('ace/mode/plain_text');
+            break;
+        case 'py':
+            editor.getSession().setMode('ace/mode/python');
+            break;
+        case 'rb':
+            editor.getSession().setMode('ace/mode/ruby');
+            break;
+        case 'c':
+        case 'cpp':
+        case 'h':
+            editor.getSession().setMode('ace/mode/c_cpp');
+            break
+        case 'html':
+            editor.getSession().setMode('ace/mode/html');
+            break;
+        case 'js':
+            editor.getSession().setMode('ace/mode/javascript');
+            break;
+        case 'md':
+            editor.getSession().setMode('ace/mode/markdown');
+            break;
+        default:
+            editor.getSession().setMode('ace/mode/plain_text');
+            break;
+    }
+}
---
+++ src/main.js
@@ -0,0 +1,47 @@
+const {app, Menu, BrowserWindow} = require('electron');
+const path = require('path');
+const url = require('url');
+
+let mainWindow;
+
+function createWindow(){
+    mainWindow  = new BrowserWindow(
+        {
+            width: 800, 
+            height: 600,
+            webPreferences: {
+                preload: `${__dirname}/preload.js`,    // preloadを追加
+                enableRemoteModule: true
+            }
+            // "icon": __dirname + "/shimarin.ico"
+        });
+
+    mainWindow.loadURL(url.format({
+        pathname: path.join(__dirname, 'index.html'),
+        protocol: 'file:',
+        slashes: true
+    }));
+
+    // it enable developing tool
+    mainWindow.webContents.openDevTools();
+
+    Menu.setApplicationMenu(null);
+    mainWindow.on('closed', () => {
+        mainWindow = null;
+    });
+}
+
+app.on('ready', createWindow);
+
+app.on('window-all-closed', () => {
+    if(process.platform !== 'darwin') {
+        app.quit();
+    }
+});
+
+app.on('activate', () => {
+    if(mainWindow === null) {
+        createWindow();
+    }
+});
+
